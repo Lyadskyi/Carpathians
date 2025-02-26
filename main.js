c451d7fd1bf40823ac7253e10770d033799fbde1
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2025-02-27 01:10:41 +0200

    Added bullets to the Gallery

--- main.js
+++ main.js
@@ -8,9 +8,26 @@ new Swiper('.image-slider', {
 		pageUpDown: true,
 	},
 	loop: true,
+	mousewheel: {
+		sensitivity: 1,
+		eventsTarget: '.image-slider',
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true,
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '">' + (index + 1) + '</span>';
+		},
+	},
 	simulateTouch: true,
 	slidesPerView: 2,
 	slideToClickedSlide: true,
 	spaceBetween: 20,
+	speed: 600,
 	touchRatio: 2,
 });
