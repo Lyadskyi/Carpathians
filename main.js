8579b4ab7de45bd062842a093f17cc2edd5bca2f
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2025-03-04 23:26:15 +0200

    Configured the Up button to appear when content is lowered

--- main.js
+++ main.js
@@ -83,3 +83,19 @@ document.addEventListener('DOMContentLoaded', () => {
 		touchRatio: 2,
 	});
 });
+
+$(window).scroll(function () {
+	let scrolled = $(window).scrollTop();
+
+	console.log(scrolled);
+
+	if (scrolled > 400) {
+		$('#back_to_top').addClass('active');
+	} else {
+		$('#back_to_top').removeClass('active');
+	}
+});
+
+$('#back_to_top').click(function () {
+	$('body,html').animate({ scrollTop: 0 }, 10);
+});
