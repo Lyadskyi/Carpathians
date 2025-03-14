20e8664d539e2e6f19806ff8d653635e548ff8b9
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2025-03-14 21:57:39 +0200

    Update @media for the mobile version

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// ? Modal window BOOK A TOUR //
+// ! Modal window BOOK A TOUR //
 
 document.addEventListener('DOMContentLoaded', () => {
 	const openModalBtn = document.querySelector('.hero-btn'); // Кнопка відкриття
@@ -49,60 +49,64 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 });
 
-// ? Modal window TOUR'S DETAILS //
+// ! Modal window TOUR'S DETAILS //
 
-// ? Side-out menu MOBILE //
+// ! Side-out menu MOBILE //
 
 document.addEventListener('DOMContentLoaded', () => {
-	const openModalBtn = document.querySelector('.hero-btn'); // Кнопка відкриття
-	const closeModalBtn = document.querySelector('.modal-close-btn'); // Кнопка закриття на X
-	const closeModalCallMeBtn = document.querySelector('.contact-form-btn'); // Кнопка закриття на кнопку CALL ME
-	const backdrop = document.querySelector('.backdrop-book-a-tour'); // Фон модального вікна BOOK A TOUR
+	const openModalMobileMenuBtn = document.querySelector('.mobile-open-btn'); // Кнопка відкриття
+	const closeModalMobileMenuBtn = document.querySelector(
+		'.mobile-menu-close-btn'
+	); // Кнопка закриття на X
+	const menu = document.querySelector('.mobile-menu');
 
-	console.log(closeModalCallMeBtn);
+	// if (!openModalMenuBtn || !closeModalMenuBtn) {
+	// 	console.error('❌ Один із елементів не знайдено!');
+	// 	return;
+	// }
 
-	if (!openModalBtn || !closeModalBtn || !backdrop) {
-		console.error('❌ Один із елементів не знайдено!');
-		return;
-	}
+	// openModalMobileMenuBtn.addEventListener('click', () => {
+	// 	menu.classList.add('is-open');
+	// });
 
 	// Функція відкриття модального вікна
-	function openModal() {
-		backdrop.classList.add('is-open');
-		body.classList.add('no-scroll'); // Заборона прокрутки
-	}
+	// function openModal() {
+	// 	openModalMobileMenuBtn.classList.add('is-open');
+	// 	mobileMenu.classList.add('no-scroll'); // Заборона прокрутки
+	// }
 
 	// Функція закриття модального вікна
-	function closeModal() {
-		backdrop.classList.remove('is-open');
-		body.classList.remove('no-scroll'); // Відновлення прокрутки
-	}
+	// function closeModal() {
+	// 	openModalMobileMenuBtn.classList.remove('is-open');
+	// 	mobileMenu.classList.remove('no-scroll'); // Відновлення прокрутки
+	// }
+
+	// closeModalMobileMenuBtn.addEventListener('click', () => {
+	// 	menu.classList.remove('is-open');
+	// });
 
 	// Обробник кліку для відкриття модального вікна
-	openModalBtn.addEventListener('click', openModal);
+	// openModalMobileMenuBtn.addEventListener('click', openModal);
 
 	// Обробник кліку для закриття модального вікна
-	closeModalBtn.addEventListener('click', closeModal);
-
-	// Обробник кліку для закриття модального вікна
-	closeModalCallMeBtn.addEventListener('click', closeModal);
+	// closeModalMenuBtn.addEventListener('click', closeModal);
 
 	// Закриття модального вікна при кліку на фон (backdrop)
-	backdrop.addEventListener('click', (event) => {
-		if (event.target === backdrop) {
-			closeModal();
-		}
-	});
+	// backdrop.addEventListener('click', (event) => {
+	// 	if (event.target === backdrop) {
+	// 		closeModal();
+	// 	}
+	// });
 
 	// Закриття модального вікна при натисканні "Escape"
-	document.addEventListener('keydown', (event) => {
-		if (event.key === 'Escape' && backdrop.classList.contains('is-open')) {
-			closeModal();
-		}
-	});
-});
+// 	document.addEventListener('keydown', (event) => {
+// 		if (event.key === 'Escape' && backdrop.classList.contains('is-open')) {
+// 			closeModal();
+// 		}
+// 	});
+// });
 
-// ? Slider swiper GALLERY //
+// ! Slider swiper GALLERY //
 
 document.addEventListener('DOMContentLoaded', () => {
 	const sliderElement = document.querySelector('.image-slider');
@@ -139,7 +143,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 });
 
-// ? Slider swiper MODAL UPCOMING TOURS //
+// ! Slider swiper MODAL UPCOMING TOURS //
 
 document.addEventListener('DOMContentLoaded', () => {
 	const sliderElementModal = document.querySelector('.modal-image-slider');
@@ -176,7 +180,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 });
 
-// ? Button UP //
+// ! Button UP //
 
 $(window).scroll(function () {
 	let scrolled = $(window).scrollTop();
